b2a7206216eb9488e8209eeef84fb569956007db
Yoni Lavi <yoni@codeinstitute.net>
2021-12-03 19:12:40 +0000

    day3 refactoring to functional style

--- day3.py
+++ day3.py
@@ -1,44 +1,42 @@
-def bits_to_rate(bits):
+def bits_to_decimal(bits):
     return int(''.join(map(str, bits)), 2)
 
 
-def bit_sum_to_common_bit(bit_sum, length, round_up=False):
-    return int(bit_sum * 2 > length or round_up and bit_sum * 2 == length)
+def sum_up_bits(bits):
+    return sum(map(int, bits))
 
 
-def day3a(input_strings):
-    digit_sums = (sum(map(int, digit_i)) for digit_i in zip(*input_strings))
-    common_bits = [bit_sum_to_common_bit(bit_sum, len(input_strings)) for bit_sum in digit_sums]
+def bit_sum_to_common_bit(bit_sum, length):
+    return int(bit_sum * 2 >= length)
 
-    gamma_rate = bits_to_rate(common_bits)
-    epsilon_rate = bits_to_rate(1-bit for bit in common_bits)
+
+def most_common_bit_in_pos(nums, pos):
+    bit_sum = sum_up_bits(num[pos] for num in nums)
+    return bit_sum_to_common_bit(bit_sum, len(nums))
+
+
+def least_common_bit_in_pos(nums, pos):
+    return 1 - most_common_bit_in_pos(nums, pos)
+
+
+def day3a(nums):
+    gamma_rate = bits_to_decimal(most_common_bit_in_pos(nums, pos) for pos in range(len(nums[0])))
+    epsilon_rate = bits_to_decimal(least_common_bit_in_pos(nums, pos) for pos in range(len(nums[0])))
     return gamma_rate * epsilon_rate
 
 
-def filter_has_most_common_bit_in_pos(nums, pos):
-    bit_sum = sum(int(num[pos]) for num in nums)
-    common_bit = bit_sum_to_common_bit(bit_sum, len(nums), round_up=True)
-    return {num for num in nums if int(num[pos]) == common_bit}
+def filter_last_one_standing(valid_nums, criterion_func, pos=0):
+    if len(valid_nums) == 1:
+        return bits_to_decimal(valid_nums[0])
 
-
-def filter_has_least_common_bit_in_pos(nums, pos):
-    bit_sum = sum(int(num[pos]) for num in nums)
-    least_common_bit = 1 - bit_sum_to_common_bit(bit_sum, len(nums), round_up=True)
-    return {num for num in nums if int(num[pos]) == least_common_bit}
-
-
-def filter_last_one_standing(nums, filter_func):
-    valid_nums = set(nums)
-    for pos in range(len(nums)):
-        valid_nums = filter_func(valid_nums, pos)
-        if len(valid_nums) == 1:
-            return bits_to_rate(valid_nums.pop())
-    raise RuntimeError("The amount of valid nums in the following set is not 1: {valid_nums}")
+    criterion_bit = criterion_func(valid_nums, pos)
+    filtered_nums = [num for num in valid_nums if int(num[pos]) == criterion_bit]
+    return filter_last_one_standing(filtered_nums, criterion_func, pos + 1)
 
 
 def day3b(input_strings):
-    oxygen_generator = filter_last_one_standing(input_strings, filter_has_most_common_bit_in_pos)
-    co2_scrubber = filter_last_one_standing(input_strings, filter_has_least_common_bit_in_pos)
+    oxygen_generator = filter_last_one_standing(input_strings, most_common_bit_in_pos)
+    co2_scrubber = filter_last_one_standing(input_strings, least_common_bit_in_pos)
     return oxygen_generator * co2_scrubber
 
 
